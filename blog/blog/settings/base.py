834e7d5db9f28b9533c90b1fd3f2320283228f6e
Christian Miranda <licmirandachristian@gmail.com>
2024-10-19 14:33:10 -0300

    modif titulo del post

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -89,16 +89,16 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'NAME': os.path.join(os.path.dirname(BASE_DIR('db.sqlite3'))),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(os.path.dirname(BASE_DIR('db.sqlite3'))),
 
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'ciberseguridad',
-         'USER': 'root',  
-         'PASSWORD': 'Florencia',
-         'HOST': '127.0.0.1',
-         'PORT': '3306',
+        #  'ENGINE': 'django.db.backends.mysql',
+        #  'NAME': 'ciberseguridad',
+        #  'USER': 'root',  
+        #  'PASSWORD': 'Florencia',
+        #  'HOST': '127.0.0.1',
+        #  'PORT': '3306',
     }
 }
 
